b592d629ef18e028d109cdb16c274c3b63b9a5fa
Luiz Felipe Cavalcanti Cornelli <lcornelli@uolinc.com>
2022-07-09 11:41:11 -0300

    create Offer e Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -26,3 +26,13 @@ INSERT INTO tb_notification (text, moment, read, route, user_id)
     VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-12T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id)
     VALUES ('Terceiro feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-14T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+
+--Table COURSE
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri)
+    VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+--Table OFFER
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id)
+    VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id)
+    VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);

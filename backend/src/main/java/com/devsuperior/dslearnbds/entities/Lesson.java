03f9eb81799d7e640d3b769dc03022bd47992a59
Luiz Felipe Cavalcanti Cornelli <lcornelli@uolinc.com>
2022-07-10 19:19:02 -0300

    create Topic

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -35,6 +35,9 @@ public abstract class Lesson implements Serializable {
     @OneToMany(mappedBy = "lesson")
     private List<Deliver> deliveries = new ArrayList<>();
 
+    @OneToMany(mappedBy = "lesson")
+    private List<Topic> topics = new ArrayList<>();
+
     public Lesson(){
     }
 
@@ -83,6 +86,10 @@ public abstract class Lesson implements Serializable {
 
     public List<Deliver> getDeliveries() {
         return deliveries;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
     }
 
     @Override

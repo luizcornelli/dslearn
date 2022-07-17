2acfc030ae82e0511ec7d8a367f58236083466ae
Luiz Cornelli <luizcornelli.dev@gmail.com>
2022-07-17 01:45:03 -0300

    Navbar styling

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
+    <>
+    <Navbar/>
     <h1>Hello DSCatalog</h1>
-  );
+    </>
+  ) ;
 }
 
 export default App;
